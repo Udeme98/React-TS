7437f5bb31d81212a93830bd310abd521d4dc830
Udeme98 <Darealudeme1@gmail.com>
2024-07-24 00:41:01 +0100

    Basic Props

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,20 @@
+type GreetingProp = {
+  name: string;
+  age: number;
+  isLoggedIn: boolean;
+};
+
+const Greeting = (prop: GreetingProp) => {
+  return (
+    <div>
+      {prop.isLoggedIn ? (
+        <h2>
+          Hello my name is {prop.name}, I am {prop.age} years!
+        </h2>
+      ) : (
+        <h2>Hello Guest</h2>
+      )}
+    </div>
+  );
+};
+export default Greeting;
